fcfda0b3a4e3c1e2e81312ec7d14f893affbf708
Mikael Lasichuk <mikael.lasichuk@yahoo.com>
2021-11-11 17:04:13 +0000

    desctructer login and register to diff files

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import tkinter as tk 
 from tkinter import *
-from tkinter import messagebox as m_box
-import bcrypt
+
+from register import register 
+from login import login
+
 import os 
 os.system('clear')
+
 
 # Create an instance of tkinter frame or window
 root = tk.Tk()
@@ -15,6 +18,7 @@ root.geometry('400x600')
 loginFrame = tk.Frame(root)
 signupFrame = tk.Frame(root)
 homeFrame = tk.Frame(root)
+
 #Pack default frame
 loginFrame.pack()
 
@@ -38,47 +42,15 @@ regPasswordInput1 = Entry(signupFrame, width=30)
 regPasswordInput1.pack()
 
 # Register logic
-def register():
-    db = open('database.txt', 'r')
-    Username = regUsernameInput.get()
-    Pasword = regPasswordInput.get()
-    Pasword1 = regPasswordInput1.get()
-    d = []
-    f = []
-    for i in db:
-        a, b = i.split(',')
-        b = b.strip()
-        d.append(a)
-        f.append(b)
-    data = dict(zip(d, f))
-
-    if not len(Username):
-        m_box.showerror('Error', 'Please enter a username')
-
-    if Pasword != Pasword1:
-        m_box.showerror('title', 'Paswords doesn\'t match!')
-         
-    else:
-        if len(Pasword) <= 6:
-            m_box.showerror('Error', 'Password too short!')
-        elif Username in d:
-            m_box.showerror('User exists!')
-        else:
-            if Pasword == Pasword1:
-                Pasword = Pasword.encode('utf-8')
-                Pasword = bcrypt.hashpw(Pasword, bcrypt.gensalt())
-
-            db = open('database.txt', 'a')
-            db.write(Username+", "+str(Pasword)+"\n")
-            m_box.showinfo('Success', 'Succes')
-
-regButton = Button(signupFrame, text='Register', command=register)
+regButton = Button(signupFrame, text='Register', command =lambda: register(
+    '', regUsernameInput, regPasswordInput, regPasswordInput1))
 regButton.pack()
 
 #Redirect to login frame function
 def goToLogin():
     loginFrame.pack(fill='both', expand=1)
     signupFrame.pack_forget()
+
 #Display to login button
 toLoginButton = Button(signupFrame, text='Login', command=goToLogin)
 toLoginButton.pack()
@@ -97,49 +69,18 @@ PasswordInput = Entry(loginFrame, width=30)
 PasswordInput.pack()
 
 #Login function
-def access():
-    db = open('database.txt', 'r')
-    Username = UsernameInput.get()
-    Pasword = PasswordInput.get()
-
-    if not len(Username or Pasword) < 1:
-        d = []
-        f = []
-        for i in db:
-            a, b = i.split(',')
-            b = b.strip()
-            d.append(a)
-            f.append(b)
-        data = dict(zip(d, f))
-
-        try:
-            if data[Username]:
-                hashed = data[Username].strip('b')
-                hashed = hashed.replace("'", "")
-                hashed = hashed.encode('utf-8')
-
-                try:
-                    if bcrypt.checkpw(Pasword.encode(), hashed):
-                        m_box.showinfo('Alert', 'Login success')
-                        homeFrame.pack(fill='both', expand=1)
-                        loginFrame.pack_forget() 
-                    else:
-                        m_box.showerror('Error', 'Wrong password')
-                except:
-                    m_box.showerror('Error', 'Incorect Password or Username')
-            else:
-                m_box.showerror('Error', 'Username doesn\'t exists')
-        except:
-            m_box.showerror('Error', 'Login error')
-
-regButton = Button(loginFrame, text='Login', command=access)
+regButton = Button(loginFrame, text='Login', command=lambda: login('',
+                                                        UsernameInput,
+                                                        PasswordInput, 
+                                                        homeFrame, 
+                                                        loginFrame))
 regButton.pack()
 
 #Sign off function
 def signOff():
     loginFrame.pack(fill='both', expand=1)
     homeFrame.pack_forget()
-    
+
 #Switch to login function
 def switchToSignIn():
     signupFrame.pack(fill='both', expand=1)
---
+++ login.py
@@ -0,0 +1,40 @@
+from tkinter import messagebox as m_box
+import bcrypt
+import tkinter as tk
+from tkinter import *
+
+def login(self, username, password, homeframe, loginframe):
+    print(1)
+    db = open('database.txt', 'r')
+    Username = username.get()
+    Pasword = password.get()
+
+    if not len(Username or Pasword) < 1:
+        d = []
+        f = []
+        for i in db:
+            a, b = i.split(',')
+            b = b.strip()
+            d.append(a)
+            f.append(b)
+        data = dict(zip(d, f))
+
+        try:
+            if data[Username]:
+                hashed = data[Username].strip('b')
+                hashed = hashed.replace("'", "")
+                hashed = hashed.encode('utf-8')
+
+                try:
+                    if bcrypt.checkpw(Pasword.encode(), hashed):
+                        m_box.showinfo('Alert', 'Login success')
+                        homeframe.pack(fill='both', expand=1)
+                        loginframe.pack_forget()
+                    else:
+                        m_box.showerror('Error', 'Wrong password')
+                except:
+                    m_box.showerror('Error', 'Incorect Password or Username')
+            else:
+                m_box.showerror('Error', 'Username doesn\'t exists')
+        except:
+            m_box.showerror('Error', 'Login error')
