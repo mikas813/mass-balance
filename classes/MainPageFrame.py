22f75bdc2e348e26d898501cc621a878c4790cdd
Mikael Lasichuk <mikael.lasichuk@yahoo.com>
2021-12-01 19:59:04 +0000

    fixed aircraft table bug, added first calc

--- classes/MainPageFrame.py
+++ classes/MainPageFrame.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from classes.PlaceholderEntry import PlaceholderEntry
 from tkinter import ttk
 import mysql.connector
+from tkinter import messagebox as m_box
 
 class MainPageFrame(tk.Frame):
 
@@ -15,12 +16,12 @@ class MainPageFrame(tk.Frame):
         pilotInput = PlaceholderEntry(self, placeholder="Pilot")
         coPilotInput = PlaceholderEntry(self, placeholder="Co-Pilot")
         baggageInput = PlaceholderEntry(self, placeholder="Baggage")
-        usableFuelInput = PlaceholderEntry(self, placeholder="Usable Fuel (F (l)*ρf(0.72) [kg]")
+        # usableFuelInput = PlaceholderEntry(self, placeholder="Usable Fuel (F (l)*ρf(0.72) [kg]")
 
         pilotInput.grid(row=2, column=1, sticky='W')
         coPilotInput.grid(row=3, column=1, sticky='W')
         baggageInput.grid(row=4, column=1, sticky='W')
-        usableFuelInput.grid(row=5, column=1, sticky='W')
+       # usableFuelInput.grid(row=5, column=1, sticky='W')
 
         # Second st row's entries
         armLabel = tk.Label(self, text="Arm (m)", font=controller.title_font)
@@ -29,24 +30,25 @@ class MainPageFrame(tk.Frame):
         pilotArmInput = PlaceholderEntry(self, placeholder="Pilot m")
         coPilotArmInput = PlaceholderEntry(self, placeholder="Co-Pilot m")
         baggageArmInput = PlaceholderEntry(self, placeholder="Baggage m")
-        fuelInputInput = PlaceholderEntry(self, placeholder="???")
+        #fuelInputInput = PlaceholderEntry(self, placeholder="???")
 
         pilotArmInput.grid(row=2, column=2, sticky='W')
         coPilotArmInput.grid(row=3, column=2, sticky='W')
         baggageArmInput.grid(row=4, column=2, sticky='W')
-        fuelInputInput.grid(row=5, column=2, sticky='W')
+        #fuelInputInput.grid(row=5, column=2, sticky='W')
 
         #Action buttons
         actionBtnsLabel = tk.Label(self, text="Action buttons", font=controller.title_font)
         actionBtnsLabel.grid(row=1, column=3, pady=20, sticky='W')
 
-        calcButton = tk.Button(self, text="Calculate")
+        calcButton = tk.Button(self, text="Calculate", command=lambda:  calculate())
 
         addAirCraftButton = tk.Button(self, text="Add Aircraft",
                            command=lambda: controller.show_frame("AircraftFrame"))
         #Action buttons
         calcButton.grid(row=4, column=3, sticky='W')
         addAirCraftButton.grid(row=5, column=3, sticky='W')
+
         # DB Connection
         database = mysql.connector.connect(host='localhost', user='root', passwd='', database='MassAndBalance')
         mycursor = database.cursor()
@@ -55,26 +57,37 @@ class MainPageFrame(tk.Frame):
         allAirCrafts = mycursor.fetchall()
         database.close()
         #Combobox
-        comboExample = ttk.Combobox(self, values=allAirCrafts)
-        comboExample.grid(row=3, column=3, sticky='W')
+        comboBox = ttk.Combobox(self, values=allAirCrafts)
+        comboBox.grid(row=3, column=3, sticky='W')
 
+        def calculate():
+            if not len(pilotInput.get()) or not len(coPilotInput.get()) or not len(baggageInput.get()) \
+                    or not len(pilotArmInput.get()) or not len(coPilotArmInput.get()) or not len(baggageArmInput.get()):
+                m_box.showerror('Error', 'All fields are required!')
+                return  False
+            if not len(comboBox.get()):
+                m_box.showerror('Error', 'Please select an aircraft!')
+                return  False
+            print('calc')
+            #Get takeoff weight conditions
 
-        #Treeview table
-        columns = ('', '', '', '')
+            # DB Connection
+            database = mysql.connector.connect(host='localhost', user='root', passwd='', database='MassAndBalance')
+            mycursor = database.cursor()
 
-        self.tree = ttk.Treeview(self, columns=columns, show='headings')
-        s = ttk.Style()
-        s.configure('Treeview', rowheight=2)
-        # define headings
+            # Get aircraft empty weight by aircraft name
+            sql = "SELECT Weight FROM Aircraft WHERE Aircraft_ID=%s"
 
-        aircrafts = []
-        aircrafts.append(('Take-off Condition', '518,3', '1,715', '889,1'))
+            #selected aircraft
+            val = (comboBox.get(),)
+            mycursor.execute(sql, val)
+            data = mycursor.fetchall()
+            emprtyAircraftWeight = data[0][0]
+            database.close()
 
+            pilotWeight = int(pilotInput.get())
+            coPilotWeight = int(coPilotInput.get())
 
-        # add data to the treeview
-        for aircraft in aircrafts:
-            self.tree.insert('', tk.END, values=aircraft)
-
-        # add a scrollbar
-        self.tree.grid(row=7, columnspan=4, pady=40, sticky='WE')
-
+            #sum aircraft empty weight + pilot w + co-pilot w
+            totalWeight = emprtyAircraftWeight + pilotWeight + coPilotWeight
+            print('Take Off conditions ',  totalWeight)
